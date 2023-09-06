e9ad669e2f3f68050cb89e2ac17454791067ffef
Daisy Cheng <yxdaisy@gmail.com>
2023-09-05 19:54:16 -0700

    Ten Words of Wisdom

--- Ten_Words_of_Wisdom/TenWordsofWisdom.cpp
+++ Ten_Words_of_Wisdom/TenWordsofWisdom.cpp
@@ -1,10 +1,43 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+int best_response(int size, int words[], int quality[])
+{
+    int max = 0, response = 0;
+    for (int i = 0; i < size; ++i)
+    {
+        if (words[i] <= 10 && max < quality[i])
+        {
+            response = i + 1;
+            max = quality[i];
+        }
+    }
+    return response;
+}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
-    std::cout << "Hello";
+    ifstream fin("input.txt");
+
+    // read and test one case
+    int cases;
+    fin >> cases;
+    for (int j = 0; j < cases; ++j)
+    {
+        int size;
+        fin >> size;
+        int *quality = new int[size];
+        int *words = new int[size];
+
+        for (int i = 0; i < size; ++i)
+        {
+            fin >> words[i] >> quality[i];
+        }
+
+        cout << best_response(size, words, quality) << endl;
+    }
+
     return 0;
 }
