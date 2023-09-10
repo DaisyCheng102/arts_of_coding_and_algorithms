9c7ac217a899251eadbfc6f267474250658f1ee3
Daisy Cheng <yxdaisy@gmail.com>
2023-09-10 16:51:55 -0700

    blank space

---
+++ Blank_Space/blank_space.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int blank_space(vector<int> arr)
+{
+    int count = 0, max = 0;
+    for(int i = 0;i < arr.size();++i)
+    {
+        if(arr[i] == 0)
+            ++count;
+        if(max < count)
+        {
+            max = count;
+            count = 0;
+        }
+    }
+    return max;
+}
+
+int main()
+{
+    ifstream fin("input.txt");
+    int cases;
+    fin >> cases;
+
+    vector<int> arr;
+    for (int i = 0; i < cases; ++i)
+    {
+        int size = 0, val;
+        fin >> size;
+        for (int j = 0; j < size; ++j)
+        {
+            fin >> val;
+            arr.push_back(val);
+        }
+
+        // <bool_expression>?true_expression : false_expression;
+        cout << blank_space(arr) << endl;
+        //must clear arr so it doesnt contain prev numbers
+        arr.clear();
+    }
+    return 0;
+}
--- Increasing/increasing.cpp
+++ Increasing/increasing.cpp
@@ -1,22 +1,55 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
+bool increasing(vector<int> arr)
+{
+    if(arr.size() == 1)
+        return true;
+    for(int i = 0; i < arr.size();++i)
+        for(int j = i + 1; j < arr.size();++j)
+            if(arr[i] != arr[j])
+                return true;
+    return false;
+}
+
 int main()
 {
-    vector<int> arr{1, 1, 1, 1};
-    for(int i = 0; i < arr.size();++i)
+    ifstream fin("input.txt");
+    int cases;
+    fin >> cases;
+
+    vector<int> arr;
+    for (int i = 0; i < cases; ++i)
     {
-        for(int j = 0; j < arr.size();++j)
+        int size = 0, val;
+        fin >> size;
+        for (int j = 0; j < size; ++j)
         {
-            if(arr[i] != arr[j])
-            {
-                cout << "YES" << endl;
-                return 0;
-            }
+            fin >> val;
+            arr.push_back(val);
         }
+
+        // <bool_expression>?true_expression : false_expression;
+        cout << (increasing(arr) ? "Yes" : "No") << endl;
+        //must clear arr so it doesnt contain prev numbers
+        arr.clear();
     }
-    cout << "NO" << endl;
     return 0;
+
+    // for(int i = 0; i < arr.size();++i)
+    // {
+    //     for(int j = 0; j < arr.size();++j)
+    //     {
+    //         if(arr[i] != arr[j])
+    //         {
+    //             cout << "YES" << endl;
+    //             return 0;
+    //         }
+    //     }
+    // }
+    // cout << "NO" << endl;
+    // return 0;
 }
